a6240254a91febaceb74be1ac530f0618172ac32
zhangxinqiang <zhangxinqiang__@hotmail.com>
2017-08-14 19:43:10 +0800

    测试6

--- test/src/Test.java
+++ test/src/Test.java
@@ -10,6 +10,8 @@ public class Test {
         System.out.println("3");
         System.out.println("2");
         System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
     }
 
     private static int getCount(Long i) {
