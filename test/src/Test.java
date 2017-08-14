18770ab315c52070e72b398f4de331beca116db4
zhangxinqiang <zhangxinqiang__@hotmail.com>
2017-08-14 19:50:12 +0800

    test分支

--- test/src/Test.java
+++ test/src/Test.java
@@ -12,6 +12,8 @@ public class Test {
         System.out.println("4");
         System.out.println("5");
         System.out.println("6");
+        System.out.println("7");
+        System.out.println("test");
     }
 
     private static int getCount(Long i) {
