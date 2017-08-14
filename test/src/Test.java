dc09a2444c946613ec776fcedbbc5bf1136f19f6
zhangxinqiang <zhangxinqiang__@hotmail.com>
2017-08-14 19:41:44 +0800

    测试提交4

--- test/src/Test.java
+++ test/src/Test.java
@@ -9,6 +9,7 @@ public class Test {
         System.out.println("1");
         System.out.println("3");
         System.out.println("2");
+        System.out.println("4");
     }
 
     private static int getCount(Long i) {
